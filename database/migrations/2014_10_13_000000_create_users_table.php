4a70e9c9a5d0586f2d61defd3cf99d0d62c22074
bartolomecantiga <cantigaonduty@gmail.com>
2024-02-18 14:31:40 +0800

    Protectiong routes with laravel sanctum

--- database/migrations/2014_10_13_000000_create_users_table.php
+++ database/migrations/2014_10_13_000000_create_users_table.php
@@ -16,16 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('type');
-            $table->string('email');
-            $table->string('address');
-            $table->string('city');
-            $table->string('state');
-            $table->string('postal_code');
-            //$table->string('email')->unique();
-            //$table->timestamp('email_verified_at')->nullable();
-            //$table->string('password');
-            //$table->rememberToken();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
